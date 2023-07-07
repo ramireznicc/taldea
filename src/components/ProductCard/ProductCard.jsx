62e79e4674702e69c23799d9f8addd41efb498a7
Nico Ramirez <ramireznicc.23@gmail.com>
2023-07-07 11:18:57 +0200

    [UI] changing product card color, border and implementing styles.js file in component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,55 +1,31 @@
 import { Link } from "react-router-dom";
-import { Paper, Box, Typography, Button, Divider } from "@mui/material";
+import { Box, Typography, Button, Divider } from "@mui/material";
 import { Send } from "@mui/icons-material";
+
+import { styles } from "./styles";
 
 export const ProductCard = ({ product }) => {
   return (
-    <Paper
-      sx={{
-        width: { xs: "100%", md: "500px" },
-        maxHeight: "140px",
-        display: "flex",
-        borderRadius: "12px",
-      }}
-    >
-      <Box
-        borderRadius="12px 0px 0px 12px"
-        width="140px"
-        height="140px"
-        component="img"
-        src={product.image}
-        sx={{ objectFit: "cover" }}
-      ></Box>
+    <Box sx={styles.card}>
+      <Box component="img" src={product.image} sx={styles.image} />
       <Box
         p="4px 8px 8px 16px"
         display="flex"
         flexDirection="column"
         flexGrow={1}
       >
-        <Typography textTransform="uppercase" variant="h6" color="primary">
+        <Typography variant="h6" sx={styles.itemTitle}>
           {product.name}
         </Typography>
-        <Typography
-          variant="caption"
-          textTransform="uppercase"
-          color="secondary"
-          fontFamily="Roboto Mono"
-          letterSpacing={1.5}
-        >
+        <Typography variant="caption" sx={styles.itemBrand}>
           {product.brand}
         </Typography>
         <Divider sx={{ py: "4px" }} flexItem />
-        <Typography
-          variant="body2"
-          sx={{ opacity: ".8", flexGrow: 1, py: "4px" }}
-        >
+        <Typography variant="body2" sx={styles.itemDescription}>
           {product.description}
         </Typography>
         <Button
-          sx={{
-            width: "fit-content",
-            alignSelf: "flex-end",
-          }}
+          sx={styles.button}
           color="secondary"
           endIcon={<Send color="primary" fontSize="small" />}
           fullWidth={false}
@@ -60,6 +36,6 @@ export const ProductCard = ({ product }) => {
           Consultar
         </Button>
       </Box>
-    </Paper>
+    </Box>
   );
 };
