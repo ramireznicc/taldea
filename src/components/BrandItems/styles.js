d523d9347baf70e228630b39ab2479834f45fb2e
Nico Ramirez <ramireznicc.23@gmail.com>
2023-07-14 17:10:17 +0200

    [UI] fixing some UI margins, sizes and layouts

--- src/components/BrandItems/styles.js
+++ src/components/BrandItems/styles.js
@@ -9,18 +9,10 @@ export const styles = {
     width: "100%",
     display: "flex",
     flexDirection: { xs: "column", md: "row" },
-<<<<<<< HEAD
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: { xs: "32px", md: "22px" },
-  },
-  image: { height: { xs: "36px", md: "48px" } },
-=======
     alignItems: { xs: "center", md: "baseline" },
     justifyContent: { xs: "space-between", md: "space-evenly" },
     gap: { xs: "44px", md: "0px" },
   },
   image: { height: { xs: "32px", md: "40px" } },
   avery: { height: { xs: "48px", md: "58px" } },
->>>>>>> develop
 };
